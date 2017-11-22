453327e69dc6ef410dc644dd30bc24c48898d95b
Gnel Simonyan <gnelsimonyan51@gmail.com>
2017-11-23 00:50:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ class Server {
 	    this.transporter = this.mail.createTransport({
 		service: 'gmail',
 		auth: {
-			user: 'snoopshakur1998@gmail.com',
-			pass: 'gnel1998'
+			user: 'asd@gmail.com',
+			pass: 'asdasd'
 		}
 	     });
 	}
